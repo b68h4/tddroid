d2bafb04d42699f7fa208958bd89ae4b6ebcfc5d
BsLinux <35398147+bsglinux16@users.noreply.github.com>
2020-08-20 14:16:01 +0300

    SSL Path Fixed

--- tdbuild/td/tddroid/libtdjsonandroid/build.sh
+++ tdbuild/td/tddroid/libtdjsonandroid/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build-$ABI
 cd build-$ABI
-cmake .. -DCMAKE_TOOLCHAIN_FILE=${ANDROID_SDK_ROOT}/build/cmake/android.toolchain.cmake -DOPENSSL_INCLUDE_DIR=third_party/crypto/$SSL_ABI/include -DOPENSSL_ROOT_DIR=third_party/crypto/$SSL_ABI  -DCMAKE_BUILD_TYPE=MinSizeRel -GNinja -DANDROID_ABI=${ABI} || exit 1
+cmake .. -DCMAKE_TOOLCHAIN_FILE=${ANDROID_SDK_ROOT}/build/cmake/android.toolchain.cmake -DOPENSSL_INCLUDE_DIR=../third_party/crypto/$SSL_ABI/include -DOPENSSL_ROOT_DIR=../third_party/crypto/$SSL_ABI  -DCMAKE_BUILD_TYPE=MinSizeRel -GNinja -DANDROID_ABI=${ABI} || exit 1
 cmake --build . || exit 1
 cd ..
 
