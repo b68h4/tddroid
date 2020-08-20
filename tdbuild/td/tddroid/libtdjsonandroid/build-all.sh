ba51f33b9bc7e1c0cbad65fa548089f40372eb0c
BsLinux <35398147+bsglinux16@users.noreply.github.com>
2020-08-20 13:05:55 +0300

    SSL_ABI Added

--- tdbuild/td/tddroid/libtdjsonandroid/build-all.sh
+++ tdbuild/td/tddroid/libtdjsonandroid/build-all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ANDROID_SDK_ROOT=../../../ndk-bundle ABI="x86" ./build.sh || exit 1
-ANDROID_SDK_ROOT=../../../ndk-bundle ABI="x86_64" ./build.sh || exit 1
-ANDROID_SDK_ROOT=../../../ndk-bundle ABI="armeabi-v7a" ./build.sh || exit 1
-ANDROID_SDK_ROOT=../../../ndk-bundle ABI="arm64-v8a" ./build.sh || exit 1
+ANDROID_SDK_ROOT=../../../ndk-bundle SSL_ABI="x86" ABI="x86" ./build.sh || exit 1
+ANDROID_SDK_ROOT=../../../ndk-bundle SSL_ABI="x86_64" ABI="x86_64" ./build.sh || exit 1
+ANDROID_SDK_ROOT=../../../ndk-bundle SSL_ABI="arm" ABI="armeabi-v7a" ./build.sh || exit 1
+ANDROID_SDK_ROOT=../../../ndk-bundle SSL_ABI="arm64" ABI="arm64-v8a" ./build.sh || exit 1
